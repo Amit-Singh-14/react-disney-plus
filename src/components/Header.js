390cd5774ddd36f461964b2f1a3a69f429bebd02
Axcel014 <amitasdf246@gmail.com>
2022-09-29 12:37:55 +0530

    Navbar-done

---
+++ src/components/Header.js
@@ -0,0 +1,106 @@
+import React from "react";
+import styled from "styled-components";
+
+function Header() {
+  return (
+    <Nav>
+      <Logo src="/images/logo.svg" />
+      <Navmenu>
+        <a>
+          <img src="/images/home-icon.svg" />
+          <span>HOME</span>
+        </a>
+        <a>
+          <img src="/images/search-icon.svg" />
+          <span>SEARCH</span>
+        </a>
+        <a>
+          <img src="/images/watchlist-icon.svg" />
+          <span>WATCHLIST</span>
+        </a>
+        <a>
+          <img src="/images/original-icon.svg" />
+          <span>ORiGINALS</span>
+        </a>
+        <a>
+          <img src="/images/movie-icon.svg" />
+          <span>MOVIES</span>
+        </a>
+        <a>
+          <img src="/images/series-icon.svg" />
+          <span>SERIES</span>
+        </a>
+      </Navmenu>
+
+      <UserImage src="https://img.freepik.com/free-photo/pleasant-looking-serious-man-stands-profile-has-confident-expression-wears-casual-white-t-shirt_273609-16959.jpg?w=2000" />
+    </Nav>
+  );
+}
+
+export default Header;
+
+const Nav = styled.nav`
+  height: 70px;
+  background: #090b13;
+  display: flex;
+  align-items: center;
+  padding: 0 36px;
+  /* justify-content: space-between; */
+`;
+
+const Logo = styled.img`
+  width: 80px;
+`;
+const Navmenu = styled.div`
+  display: flex;
+  /* align-items: center; */
+  flex: 1;
+  margin-left: 25px;
+
+  a {
+    display: flex;
+    align-items: center;
+    padding: 0 10px;
+    cursor: pointer;
+
+    img {
+      width: 20px;
+    }
+
+    span {
+      font-size: 13px;
+      letter-spacing: 1.42px;
+      position: relative;
+
+      &:after {
+        margin-top: 5px;
+        content: "";
+        height: 2px;
+        background: #fff;
+        position: absolute;
+        left: 0;
+        right: 0;
+        bottom: -6px;
+        /* transform-origin: center; */
+        opacity: 0;
+        transform: scaleX(0);
+      }
+    }
+
+    &:hover {
+      span:after {
+        transform: scaleX(1);
+        transition: transform 0.4s ease;
+        opacity: 1;
+      }
+    }
+  }
+`;
+
+const UserImage = styled.img`
+  width: 50px;
+  height: 50px;
+  object-fit: cover;
+  border-radius: 50%;
+  cursor: pointer;
+`;
