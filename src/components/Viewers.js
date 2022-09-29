fda5686d41502cd9534dd01696ff8546275e03b6
Axcel014 <amitasdf246@gmail.com>
2022-09-29 18:26:45 +0530

    add Viewer

---
+++ src/components/Viewers.js
@@ -0,0 +1,53 @@
+import React from "react";
+import styled from "styled-components";
+
+function Viewers() {
+  return (
+    <Container>
+      <Wrap>
+        <img src="/images/viewers-disney.png" />
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-pixar.png" />
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-marvel.png" />
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-starwars.png" />
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-national.png" />
+      </Wrap>
+    </Container>
+  );
+}
+
+export default Viewers;
+
+const Container = styled.div`
+  margin-top: 30px;
+  display: grid;
+  padding: 30px 0 26px;
+  grid-gap: 25px;
+  grid-template-columns: repeat(5, minmax(0, 1fr));
+`;
+
+const Wrap = styled.div`
+  box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
+    rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+  border-radius: 10px;
+  cursor: pointer;
+  border: 3px solid rgba(249, 249, 249, 0.1);
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  &:hover {
+    transition-duration: 0.3s;
+    transform: scale(1.13);
+    border-color: rgba(249, 249, 249, 0.8);
+  }
+`;
